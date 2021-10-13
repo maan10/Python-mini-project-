77a5f493eb61785b529ac8177d8abd8ef57436a9
Mohammad Usman <90419764+maan10@users.noreply.github.com>
2021-10-14 01:40:37 +0500

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,37 @@
+# string concatenation 
+"""
+-----------------------------------------------------------------------
+name="Hi Umair good morning "
+
+greeting="How are you"
+ok="fine"
+#firt way
+print(f"{name} hope everything quite nice" )
+
+#2nd way
+print(name+" "+"hope everything quite nice" )
+
+#3rd way
+
+print("{} hope everything quite nice {} everthing  {}?".format(name,greeting,ok))
+
+Code by: maan
+Date: 16/09/2021
+------------------------------------------------------------------------------------
+"""
+
+
+"""
+                    ---------------------
+                    *****Project 1:*****
+                    ---------------------
+"""
+
+adj=input("Adjective:")
+verb1=input("Verb: ")
+verb2=input("verb: ")
+famous_person=input("Famous person: ")
+
+madlib=f"Computer programmind is o {adj}! it make  me so excited  all the time because i love to {verb1}.stay hydrated and {verb2} like you are "
+
+print(madlib)
